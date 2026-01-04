b53e593af13602a3a680fab161fa27ea9c5a65c9
antau <antau43@gmail.com>
2026-01-03 19:15:36 -0600

    Update tcgp_email_parser.py

--- tcgp_email_parser.py
+++ tcgp_email_parser.py
@@ -1,49 +1,53 @@
+import streamlit as st
 import streamlit.components.v1 as components
 
-if orders:
-    table_html = """
-    <style>
-        table {
-            font-family: Arial, sans-serif;
-            font-size: 10pt;
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
-            border: 1px solid #cccccc;
-            padding: 4px 6px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        a {
-            color: #0066cc;
-            text-decoration: none;
-        }
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
-    <table>
-        <tr>
-            <th>Store Name</th>
-            <th>Order Total</th>
-        </tr>
-    """
-
-    for o in orders:
-        table_html += f"""
+def render_table(orders):
+    rows_html = "".join(
+        f"""
         <tr>
             <td><a href="{o['url']}" target="_blank">{o['store']}</a></td>
             <td>{o['total']}</td>
         </tr>
         """
-
-    table_html += "</table>"
-
-    components.html(
-        table_html,
-        height=700,
-        scrolling=True
+        for o in orders
     )
+
+    full_html = f"""
+    <style>
+        table {{
+            font-family: Arial, sans-serif;
+            font-size: 10pt;
+            border-collapse: collapse;
+            width: 100%;
+        }}
+        th, td {{
+            border: 1px solid #cccccc;
+            padding: 4px 6px;
+            text-align: left;
+        }}
+        th {{
+            background-color: #f2f2f2;
+        }}
+        a {{
+            color: #0066cc;
+            text-decoration: none;
+        }}
+        a:hover {{
+            text-decoration: underline;
+        }}
+    </style>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Store Name</th>
+                <th>Order Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            {rows_html}
+        </tbody>
+    </table>
+    """
+
+    components.html(full_html, height=600, scrolling=True)
